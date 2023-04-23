8278fe857d160c1ff87fc900466ccf7e6127f8e4
Clockbomb <mrgo2000@gmail.com>
2023-04-23 20:51:27 +0100

    run simulations and write files separated

--- genFiles_script.py
+++ genFiles_script.py
@@ -4,28 +4,6 @@ import random
 import os
 random.seed(10)
 
-
-#-----------------------------------------------------------------------------------------------------
-def random_kset(index1, index2, index3): #, index3, index4, index5, index6, index7):
-    k_range = 10
-    
-    list=[]
-    for item in k:
-        list.append(np.full(n_trainSet, item ))   
-    
-    array_constant = np.array(list) # pass to array
-
-
-    array_random = np.random.uniform(1,10, size = (k.size, n_trainSet))
-    # array_random = np.random.lognormal(0,3,n_trainSet)*k_range
-    #print(array_random)
-
-    array_constant[index1]  =  array_random[index1]*k[index1]
-    array_constant[index2]  =  array_random[index2]*k[index2] 
-    array_constant[index3]  =  array_random[index3]*k[index3]
-
-    return np.transpose(array_constant)
-
 #-----------------------------------------------------------------------------------------------------
 
 class Parameters():
@@ -62,39 +40,39 @@ class Parameters():
     
 
 
-def runSimulations(k_set, filename = 'datapoints.txt'):
+def runSimulations(k_set, loki_path):
     #------------------Generate the Chemistry + SetUp files--------------------------#
+    # get the path of the current working directory
+    cwd = os.getcwd()
 
-    # Read in the files
-    with open('C:\\Users\\clock\\Desktop\\Python\\O2_simple_1.chem', 'r') as file :
+    # Read in the example files
+    with open(cwd + '\\O2_simple_1.chem', 'r') as file :
         lines = []
         for line in file:
             lines.append(line.strip())
         chemFiledata = lines
 
 
-    with open('C:\\Users\\clock\\Desktop\\Python\\setup_O2_simple.in', 'r') as file :
-        setup_data = file.read() # for the setup files we dont need to separate the string in lines
+    with open(cwd + '\\setup_O2_simple.in', 'r') as file :
+        setup_data = file.read() # (for the setup files we dont need to separate the string in lines)
 
     list_out = []
     # First substitution: replace the values of array k
     for i in range(len(k_set[0])): 
         # Replace the target string
-        #chemFiledata = chemFiledata.replace("{:.2E}".format(k[i]), "{:.5E}".format(k_set[0][i]))
         line = chemFiledata[i].split()
         line[-2] = "{:.4E}".format(k_set[0][i])
         list_out.append('  '.join(line))
 
 
     # Write the out chem file data again
-    #outfile = open('C:\\Users\\clock\\Desktop\\Python\\ChemFiles\\O2_simple_1_0.chem', 'w')
-    outfile = open('C:\\Users\\clock\\Desktop\\LoKI_v3.1.0\\Code\\Input\\SimplifiedOxygen\\O2_simple_1_0.chem', 'w')
+    outfile = open(loki_path+ '\\Code\\Input\\SimplifiedOxygen\\O2_simple_1_0.chem', 'w')
     outfile.write("\n".join(list_out))
 
     # First substitution of the SetUp file
-    setup_data = setup_data.replace('O2_simple_1.chem', 'O2_simple_1_0.chem')
-    setup_data = setup_data.replace('OxygenSimplified_1', 'OxygenSimplified_1_0')
-    outfile = open('C:\\Users\\clock\\Desktop\\LoKI_v3.1.0\\Code\\Input\\SimplifiedOxygen\\setup_O2_simple_0.in', 'w')
+    setup_data = setup_data.replace('O2_simple_1.chem', 'O2_simple_1_0.chem') # change the name of the chem file to read
+    setup_data = setup_data.replace('OxygenSimplified_1', 'OxygenSimplified_1_0') # change the name of the output folder
+    outfile = open(loki_path+ '\\Code\\Input\\SimplifiedOxygen\\setup_O2_simple_0.in', 'w')
     outfile.write(setup_data)
 
     # Then replace for all k_set
@@ -102,13 +80,11 @@ def runSimulations(k_set, filename = 'datapoints.txt'):
         list_out = []
         for i in range(len(k_set[j])): # for each k (for each line in file)
             # Replace the target string
-            #chemFiledata = chemFiledata.replace("{:.5E}".format(k_set[j-1][i]), "{:.5E}".format(k_set[j][i]))
             line = chemFiledata[i].split()
             line[-2] = "{:.4E}".format(k_set[j][i])
             list_out.append('  '.join(line))
 
-        #outfile = open('C:\\Users\\clock\\Desktop\\Python\\ChemFiles\\O2_simple_1_' +str(j)+'.chem', 'w')
-        outfile = open('C:\\Users\\clock\\Desktop\\LoKI_v3.1.0\\Code\\Input\\SimplifiedOxygen\\O2_simple_1_' +str(j)+'.chem', 'w')
+        outfile = open(loki_path+ '\\Code\\Input\\SimplifiedOxygen\\O2_simple_1_' +str(j)+'.chem', 'w')
         outfile.write("\n".join(list_out))
         outfile.close()
 
@@ -118,10 +94,6 @@ def runSimulations(k_set, filename = 'datapoints.txt'):
         outfile = open('C:\\Users\\clock\\Desktop\\LoKI_v3.1.0\\Code\\Input\\SimplifiedOxygen\\setup_O2_simple_' +str(j)+'.in', 'w') 
         outfile.write(setup_data)
         outfile.close()
-
-
-    #---------------------------------------------------
-    # Save histograms of k's distr.
 
 
 
@@ -136,11 +108,8 @@ def runSimulations(k_set, filename = 'datapoints.txt'):
     #--------------------------------------------Retrieve output data-----------------------------------------------------------------------
     #------------------------------------------------------------------------------------------------------------------------------
     #------------------------------------------------------------------------------------------------------------------------------
-
+def writeDataFile(npoints, species, loki_path, filename = 'datapoints.txt'):
     #species = ['O2(X)','O2(a)', 'O2(b)', 'O(3P)', 'O(1D)']
-
-    species = ['O2(X)','O2(a)', 'O(3P)']
-
     def read_output(file_address):
 
         with open(file_address, 'r') as file :
@@ -154,20 +123,20 @@ def runSimulations(k_set, filename = 'datapoints.txt'):
                 s+=1  # note: species does not go out of index because filedata ends
         return list
 
-    # C:\\Users\\clock\\Desktop\\LoKI_v3.1.0\\Code\\Output
     densities =[]
     # Read data from all output folders
-    for i in range(len(k_set)):
-        file_address = 'C:\\Users\\clock\\Desktop\\LoKI_v3.1.0\\Code\\Output\\OxygenSimplified_1_' + str(i) + '\\chemFinalDensities.txt'
+    for i in range(npoints):
+        file_address = loki_path+ '\\Code\\Output\\OxygenSimplified_1_' + str(i) + '\\chemFinalDensities.txt'
         densities.append(read_output(file_address))
 
 
     densities = np.array(densities)
     #print(densities)
 
-
-    with open('C:\\Users\\clock\\Desktop\\Python\\'+ filename, 'w') as file:
-        for i in range(len(k_set)):
+    # get the path of the current working directory
+    cwd = os.getcwd()
+    with open(cwd + filename, 'w') as file:
+        for i in range(npoints):
             k_line = k_set[i]
             densitie_line = densities[i]
 
@@ -178,23 +147,25 @@ def runSimulations(k_set, filename = 'datapoints.txt'):
 
 
 if __name__ == '__main__':
+
+    # Get current working directory
+    cwd = os.getcwd()
+    loki_path = "C:\\Users\\clock\\Desktop" + '\\LoKI_v3.1.0'
+
     # Definition of scheme 
     k = np.array([6E-16,1.3E-15,9.6E-16,2.2E-15,7E-22,3E-44,3.2E-45,5.2,53]) # total of 9 reactions
-    k_columns = [0,1,2] # if none change all rate coefficients
+    species = ['O2(X)','O2(a)', 'O(3P)']
+    k_columns = [0,1,2] # if none, changes all rate coefficients
     n_react = k.size
-    n_trainSet = 4
-    randOn = True
+    n_trainSet = 10
 
     # Create training dataset
-    k_set_training = random_kset(0,1,2)
-    # print("k_set lenght: ", len(k_set_training))
-
     parameters = Parameters(k, npoints=n_trainSet, krange=[1,10])
     parameters.random_kset(k_columns)
 
-    # test if parameters.k_set is equal to k_set_training
-    print("k_set_training: ", k_set_training)
-    print("parameters.k_set: ", parameters.k_set)
+    # Run simulations
+    runSimulations(parameters.k_set, loki_path)
+    writeDataFile(species, loki_path, filename='datapoints.txt')
 
     # runSimulations(k_set_training)
     # writeDataFiles(n_trainSet)
