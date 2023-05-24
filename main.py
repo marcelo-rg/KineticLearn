bccd3e8703a98148404be199c77204f4210b6650
marcelo-rg <mrgo2000@gmail.com>
2023-05-24 11:49:22 +0100

    rename Train->Trainer file

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-from src.Model import NSurrogatesModel
-from src.DataHandler import LoadDataset
-from src.Train import NSurrogatesModelTrainer
+import torch
+from torch.nn import MSELoss
+from torch.optim import Adam
 
-import torch 
-import torch.nn as nn
+from Model import NSurrogatesModel
+from DataHandler import LoadDataset
+from Trainer import NSurrogatesModelTrainer
 
 # Specify device
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Define the model parameters
+input_size = 3  # example
+output_size = 3  # example
+hidden_size = (100,)  # example
+n_surrog = 2  # example
+epochs = 10  # example
 
 # Initialize your model
 model = NSurrogatesModel(input_size, output_size, hidden_size, n_surrog)
@@ -15,11 +23,13 @@ model = NSurrogatesModel(input_size, output_size, hidden_size, n_surrog)
 datasets = [LoadDataset(src_file=f"data/datapoints_pressure_{i}.txt", nspecies=3) for i in range(n_surrog)]
 
 # Specify loss function and optimizer
-criterion = torch.nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+criterion = MSELoss()
+optimizer = Adam(model.parameters(), lr=0.001)
 
 # Create trainer
-trainer = NSurrogatesModelTrainer(model, datasets, device, criterion, optimizer)
+trainer = NSurrogatesModelTrainer(model, datasets, device, criterion, criterion, optimizer)
 
-# Train model
-trainer.train_model(epochs)
+# Train surrogate models
+trainer.train_surrg_models(epochs)
+
+# Further training can be done here
