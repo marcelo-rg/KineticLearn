2fea97cfbaee2d0e47da555ed6d865361bb2127e
marcelo-rg <mrgo2000@gmail.com>
2023-05-25 01:09:34 +0100

    problem: need the training normalization transformation

--- main.py
+++ main.py
@@ -7,19 +7,22 @@ from src.DataHandler import LoadDataset
 from src.Trainer import NSurrogatesModelTrainer
 from src.PlottingTools import PlottingTools
 
+# recover reproducibility
+torch.manual_seed(0)
+
 # Specify device
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Specify number of surrogate models and densities
 n_surrog = 1  # example
-n_param = 3 # number of densities
+n_param = 3 # number of input densities
 k_columns = [0,1,2]
 
 # Define the model parameters
 input_size = n_surrog*n_param  # example
 output_size = 3  # number of coefficients
 hidden_size = (10,10)  # architecture of the main model
-max_epoch = 40  # example
+max_epoch = 50  # example
 
 # Initialize your model
 model = NSurrogatesModel(input_size, output_size, hidden_size, n_surrog)
@@ -41,6 +44,17 @@ training_losses, validation_losses = trainer.train_surrg_models(max_epoch)
 
 # Further training ...
 
+# for evaluation, move model to cpu
+if device.type == 'cuda':  
+    model = model.to('cpu')
+
 # Plot training and validation loss histories
 plotter = PlottingTools()
 plotter.plot_loss_history(training_losses, validation_losses)
+
+# Get first surrogate model
+surrogate_model = model.surrog_nets[0]
+
+# Plot validation using test dataset
+test_dataset = LoadDataset(src_file="data/datapoints_pressure_0_test.txt", nspecies=3, react_idx=k_columns)
+plotter.plot_predictions(surrogate_model, test_dataset)
--- src/PlottingTools.py
+++ src/PlottingTools.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import os
+import numpy as np
+import torch
 
 class PlottingTools:
     def plot_loss_history(self, training_losses, validation_losses):
@@ -7,12 +9,60 @@ class PlottingTools:
         if not os.path.exists('images'):
             os.makedirs('images')
 
+        n_epochs = len(training_losses['surrogate_0'])
+
         for i in range(len(training_losses)):
             plt.figure(i)
-            plt.plot(training_losses[f'surrogate_{i}'], label='Training Loss')
-            plt.plot(validation_losses[f'surrogate_{i}'], label='Validation Loss')
+            plt.plot(np.arange(1,n_epochs+1,1),training_losses[f'surrogate_{i}'],"-o", markersize=4, label='Training Loss')
+            plt.plot(np.arange(1,n_epochs+1,1),validation_losses[f'surrogate_{i}'], "-o", markersize=4, label='Validation Loss')
             plt.xlabel('Epoch')
             plt.ylabel('Loss')
             plt.title(f'Surrogate {i} Loss History')
             plt.legend()
             plt.savefig(f'images/surrogate_{i}_loss_history.png')
+
+
+    def plot_predictions(self, model, test_dataset):
+        model.eval()  # Switch to evaluation mode
+
+        # Make sure the data is on the CPU
+        test_data = test_dataset[:][0].to("cpu")
+        test_targets = test_dataset[:][1].to("cpu")
+
+        with torch.no_grad():  # Disable gradient calculation
+            predictions = model(test_data)
+
+        # Convert tensors to numpy arrays
+        predictions = predictions.numpy()
+        true_values = test_targets.numpy()
+
+        # Plot for each species
+        species = ['O2(X)','O2(a)', 'O(3P)']
+        fig, axs = plt.subplots(1, 3, figsize=(15,5))
+        for i, ax in enumerate(axs):
+            ax.scatter(true_values[:, i], predictions[:, i])
+            ax.set_xlabel('True Values')
+            ax.set_ylabel('Predictions')
+            # Add a diagonal line representing perfect agreement
+            ax.plot([0, 1], [0, 1], linestyle='--', color='k')
+            ax.set_title(f'True Values vs Predictions for {species[i]}')
+
+            # rel_err = np.abs(np.subtract(true_values[:, i], predictions[:, i])/true_values[:, i])
+
+            # textstr = '\n'.join((
+            # r'$Mean\ \epsilon_{rel}=%.2f$%%' % (rel_err.mean()*100, ),
+            # r'$Max\ \epsilon_{rel}=%.2f$%%' % (max(rel_err)*100, )))
+
+            # # colour point o max error
+            # max_index = np.argmax(rel_err)
+            # ax.scatter(true_values[max_index,i],predictions[max_index,i] , color="gold", zorder= 2)
+
+            # # these are matplotlib.patch.Patch properties
+            # props = dict(boxstyle='round', alpha=0.5) #, facecolor='none', edgecolor='none')
+
+            # # place a text box in upper left in axes coords
+            # ax.text(0.70, 0.25, textstr, fontsize=14,  transform=plt.gca().transAxes,
+            #     verticalalignment='top', bbox=props)
+
+        plt.tight_layout()
+        plt.savefig('images/predictions_vs_true_values.png')
